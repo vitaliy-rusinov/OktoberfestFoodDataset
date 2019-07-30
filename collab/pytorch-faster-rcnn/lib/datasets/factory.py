d7b899a18a035c469230899ed298051a21e8a050
vitaliy-rusinov <vitaliy.rusinov.cv@gmail.com>
2019-07-30 23:47:25 +0200

    Update factory.py

--- collab/pytorch-faster-rcnn/lib/datasets/factory.py
+++ collab/pytorch-faster-rcnn/lib/datasets/factory.py
@@ -24,10 +24,10 @@ for year in ['2007', '2012']:
 '40_test_large','40_test_nightmode','40_trainval','60_test','60_test_large','60_test_nightmode',
 '60_trainval','80_test','80_test_large','80_test_nightmode','80_trainval','daynight_test',
 'daynight_test_large','daynight_test_nightmode','daynight_trainval','test','test_large','test_nightmode',
-'trainval','10_vid_test','10_vid_test_nightmode.txt','10_vid_test_small.txt','10_vid_trainval.txt',
-'40_vid_test','40_vid_test_nightmode.txt','40_vid_test_small.txt','40_vid_trainval.txt',
-'60_vid_test','60_vid_test_nightmode.txt','60_vid_test_small.txt','60_vid_trainval.txt',
-'80_vid_test','80_vid_test_nightmode.txt','80_vid_test_small.txt','80_vid_trainval.txt',
+'trainval','10_vid_test','10_vid_test_nightmode','10_vid_test_small','10_vid_trainval',
+'40_vid_test','40_vid_test_nightmode','40_vid_test_small','40_vid_trainval',
+'60_vid_test','60_vid_test_nightmode','60_vid_test_small','60_vid_trainval',
+'80_vid_test','80_vid_test_nightmode','80_vid_test_small','80_vid_trainval',
 ]:
     name = 'voc_{}_{}'.format(year, split)
     __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
